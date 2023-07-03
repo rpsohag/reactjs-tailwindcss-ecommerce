622a88b004fd1403e1c5b4974189241cfe11b1e8
RP SOHAG <rpsohag.bd@gmail.com>
2023-07-03 10:38:40 +0600

    header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { SidebarContext } from '../contexts/SidebarContext'
 import { CartContext } from '../contexts/CartContext'
 import { BsBag } from 'react-icons/bs'
+import Logo from './../img/logo.png'
 
 const Header = () => {
-  const {isOpen, setIsOpen} = useContext(SidebarContext)
+  const {isOpen, setIsOpen} = useContext(SidebarContext);
+  const [isActive, setIsActive] = useState(false)
   const {itemAmount} = useContext(CartContext)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false)
+    })
+  })
   return (
-    <div>
-      <div>Header</div>
-      <button onClick={() => setIsOpen(!isOpen)}><BsBag className='text-2xl'/></button>
-      <div>{itemAmount}</div>
-    </div>
+    <header className={`${isActive ? 'bg-white shadow-md' : 'bg-none py-6'} fixed w-full z-10 transition-all py-3`}>
+      <div className='container flex mx-auto items-center justify-between h-full'>
+          <Link to="/">
+            <div>
+            <img className='w-[40px]' src={Logo} alt="" />
+            </div>
+          </Link>
+        <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer flex relative max-w-[50px] '>
+          <BsBag className='text-2xl'/>
+            <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full justify-center items-center flex'>{itemAmount}</div>
+        </div>
+      </div>
+    </header>
   )
 }
 
